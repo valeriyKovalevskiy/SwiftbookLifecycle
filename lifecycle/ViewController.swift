e2762b93d6cecdfb8951372789a9d85fa2a68e08
Valeriy Kovalevskiy <kovalevskiy2021@gmail.com>
2020-02-17 11:27:15 +0300

    Initial Commit

---
+++ lifecycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lifecycle
+//
+//  Created by Valeriy Kovalevskiy on 2/17/20.
+//  Copyright © 2020 v.kovalevskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
